5639d259676f30bfc395c1199f3af32f122cc953
Jacob Edward Dawson <reviber8@gmail.com>
2017-08-12 12:31:17 +0200

    started readreddit project w/ angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Readreddit is live...kind of...';
 }

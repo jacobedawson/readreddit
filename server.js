145852622faf52b99b1c51ada8beec7cdcd0d7fb
Jacob Edward Dawson <reviber8@gmail.com>
2017-08-12 18:12:35 +0200

    added url parsing, retrieving posts from reddit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const snow = require('snoowrap'); // Reddit API wrapper
+const fs = require('fs');
+const getURLs = require('get-urls');
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+// TODO remove hardcoded credentials
+const r = new snow({
+  userAgent: 'Get book lists',
+  clientId: 'FyApUjvFK5lH3Q',
+  clientSecret: '4Niv_Z2ZtIqwIixSRVN3a9bxlyI',
+  username: 'jacobedawson',
+  password: 'DYb&H^Hd3#7b'
+});
+
+// The following function retrieves the top 100 posts in a subreddit
+// for the past week 
+// r.getSubreddit('startups')
+//   .getTop({
+//     limit: 100,
+//     time: 'week'
+//   })
+//   .then()
+//   .map((post, index) => {
+//     console.log(post);
+//     return {
+//       index,
+//       title: post.title,
+//       id: post.id,
+//       score: post.score,
+//       url: post.url,
+//       author: post.author,
+//       comments: post.num_comments
+//     }
+//   })
+//   .then(post => {
+//     writeFile(JSON.stringify(post), 'top-posts','json');
+//   });
+
+// This function will retrieve a reddit post by id
+// then save it to a file for search processing
+r.getSubmission('6s6r4a')
+    .fetch()
+    .then(post => checkPostForBookLinks(post));
+
+function checkPostForBookLinks(post) {
+    const text = JSON.stringify(post).replace('\\"','');
+    const x = getURLs(text);
+    for (let link of x) {
+        if (!link.includes('amazon')) {
+            x.delete(link);
+        }
+        if (link.includes('a%3') || link.includes('n-')) {
+            x.delete(link);
+        }
+    }
+    console.log(x);
+}
+
+
+// TASKS
+
+// 2.) Get all threads & comments? for the past 7 days / 168 hours
+// 3.) Process all the links in the text, extract any that point to Amazon, Safari or O'Reilly
+// 4.) Verify that the link is to a book page (via Amazon Product API / metadata)
+
+function writeFile(data, filename = 'temp', extension) {
+  const path = `./server/processing/${filename}.${extension}`;
+  fs.writeFile(path, data, (err) => {
+    if (err) console.log(err);
+    console.log('Successfully wrote file');
+  });
+}
+
+app.listen(3000, () => {
+  console.log('App listening on 3000');
+});
